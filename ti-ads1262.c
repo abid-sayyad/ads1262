ee1286cf64d629e4f4799c6a705d9a87c9a86419
abid-sayyad <sayyad.abid16@gmail.com>
2024-10-01 10:36:42 +0530

    Add ADS1262 register addresses

--- ti-ads1262.c
+++ ti-ads1262.c
@@ -22,6 +22,7 @@
 #include <asm/unaligned.h>
 
 /* Commands */
+#define ADS1262_CMD_NOP		0x00
 #define ADS1262_CMD_RESET       0x06
 #define ADS1262_CMD_START1      0x08
 #define ADS1262_CMD_RDATA1      0x12
@@ -31,7 +32,26 @@
 
 /* Registers */
 #define ADS1262_REG_ID          0x00
+#define ADS1262_REG_POWER       0x01
+#define ADS1262_REG_INTERFACE   0x02
+#define ADS1262_REG_MODE0       0x03
+#define ADS1262_REG_MODE1       0x04
+#define ADS1262_REG_MODE2       0x05
 #define ADS1262_REG_INPMUX      0x06
+#define ADS1262_REG_OFCAL0      0x07
+#define ADS1262_REG_OFCAL1      0x08
+#define ADS1262_REG_OFCAL2      0x09
+#define ADS1262_REG_FSCAL0      0x0A
+#define ADS1262_REG_FSCAL1      0x0B
+#define ADS1262_REG_FSCAL2      0x0C
+#define ADS1262_REG_IDACMUX     0x0D
+#define ADS1262_REG_IDACMAG     0x0E
+#define ADS1262_REG_REFMUX      0x0F
+#define ADS1262_REG_TDACP       0x10
+#define ADS1262_REG_TDACN       0x11
+#define ADS1262_REG_GPIOCON     0x12
+#define ADS1262_REG_GPIODIR     0x13
+#define ADS1262_REG_GPIODAT     0x14
 
 /* ADS1262_SPECS */
 #define ADS1262_MAX_CHANNELS    11
